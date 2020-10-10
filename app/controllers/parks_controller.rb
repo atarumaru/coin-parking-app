7b1eb3e775137e500c3522bb0a964f3700d9e1e0
wataru-a-72 <now.or.never34@icloud.com>
2020-10-10 23:23:21 +0900

    駐輪番号入力機能実装

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,6 +4,24 @@ class ParksController < ApplicationController
   end
 
   def new
-    @number = Park.new
+    @park = Park.new
+  end
+
+  def create
+    @park = Park.create(park_params)
+    if @park.save
+      if @park.id != 1
+      @pronumber = Park.first
+      @pronumber.delete
+      redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    end
+  end
+
+  private
+  def park_params
+    params.require(:park).permit(:number)
   end
 end

2737d862ac2548453701b8ced523e300fd00c0b7
wataru-a-72 <now.or.never34@icloud.com>
2020-10-27 00:50:40 +0900

    ポート番号の指定を変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,9 +10,6 @@ working_directory "#{app_path}/current"
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
-listen 3000
-
 listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
